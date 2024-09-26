7ddceca8118a178c8b1604a01b1e97658cbd7e64
baker busulwa <bbusulwa58@gmail.com>
2024-09-26 11:21:00 -0700

    added the scr folder

---
+++ src/Calc.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Calc {
+
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        int result;
+
+        Scanner calc = new Scanner(System.in);
+        System.out.println("Enter first num");
+        num1=calc.nextInt();
+        System.out.println("Enter second num");
+        num2=calc.nextInt();
+        result = num1 * num2;
+    }
+}
